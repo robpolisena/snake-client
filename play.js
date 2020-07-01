d770922f727b8214c454ff9c1df02aa4036437ac
Rob <robpolisena@gmail.com>
2020-07-01 16:32:30 +0000

    Snake Client setup

--- play.js
+++ play.js
@@ -4,19 +4,19 @@ const net = require('net');
  * Establishes connection with the game server
  */
 const connect = function() {
-  const conn = net.createConnection({ 
+  const conn = net.createConnection({
     host: '135.23.222.148',
     port: 50541
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
 
   conn.on('data', (data) => {
-    console.log('Message from client: ', data)
+    console.log('Message from client: ', data);
   });
 
   return conn;
-}
+};
 
 console.log('Connecting ...');
 console.log('hello');
