5cdb3b1387acf8934b853d3091782e396b2c83a7
Rob <robpolisena@gmail.com>
2020-07-01 17:34:58 +0000

    Snake added connection module

--- play.js
+++ play.js
@@ -1,24 +1,8 @@
-const net = require('net');
+const { connect } = require('./client');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({
-    host: '135.23.222.148',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
 
-  conn.on('data', (data) => {
-    console.log('Message from client: ', data);
-  });
-
-  return conn;
-};
 
 console.log('Connecting ...');
-console.log('hello');
+//console.log('hello');
 
 connect();
