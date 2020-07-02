1b452836441bac9a020685f16a7eb51f991f7099
Rob <robpolisena@gmail.com>
2020-07-02 00:21:39 +0000

    added movement commands

--- input.js
+++ input.js
@@ -2,24 +2,44 @@
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+  let w = 'Move: up';
+  let s = 'Move: down';
+  let a = 'Move: left';
+  let d = 'Move: right';
+  
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', (key) => {
     handleUserInput(key);
+    if (key === 'a') {
+      connection.write(a);
+    }
+    if (key === 's') {
+      connection.write(s);
+    }
+    if (key === 'w') {
+      connection.write(w);
+    }
+    if (key === 'd') {
+      connection.write(d);
+    }
   });
   return stdin;
 };
-
 
 const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
   }
 };
-
 
 const setIn = {
   setupInput
